c6325cefeab21e2baeb164216cfbfe5e659419df
Jerad Jacob <jeradj@gmail.com>
2018-01-02 14:17:07 -0500

    Incremented version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'liudasbk@users.noreply.github.com'
 license 'MIT'
 description 'Installs/Configures pulp_server'
 long_description 'Installs/Configures pulp_server'
-version '0.2.0'
+version '0.2.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/liudasbk/pulp-server-cookbook/issues'
 source_url 'https://github.com/liudasbk/pulp-server-cookbook'
